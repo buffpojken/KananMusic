a34a8637a2394bb6ed4611261da5a7d32e1bee6c
buffpojken <daniel@sykewarrior.com>
2012-08-04 14:46:39 +0200

    added web-gui

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'eventmachine', '1.0.0.rc.4'
 gem 'json'
+gem 'eventmachine_httpserver'
--- bunny.rb
+++ bunny.rb
@@ -1,11 +1,29 @@
 require 'eventmachine'
 require 'json'
+require 'evma_httpserver'
 
-cl = UDPSocket.new
-cl.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR,1)
-cl.setsockopt(Socket::SOL_SOCKET,Socket::SO_BROADCAST,1)
-cl.bind('0.0.0.0', 8282)                                
-                           
+class APIServer < EM::Connection
+  include EM::HttpServer
+
+  def post_init
+    super
+    no_environment_strings
+  end        
+
+  def process_http_request
+    params = CGI::parse((@http_query_string || ""))          
+    EM.system("mplayer #{params['sound'][0]}") do |output, status|
+      puts "Done"
+    end
+    response = EM::DelegatedHttpResponse.new(self)
+    response.status = 200
+    response.content_type 'text/javascript'     
+    status = {:status => "ok"}
+    response.content = "#{params["callback"][0]}(#{status.to_json})"
+    response.send_response      
+  end
+end
+
 
 class MusicPlayer < EM::Connection  
   def receive_data(data)           
@@ -13,12 +31,22 @@ class MusicPlayer < EM::Connection
     data = JSON.parse(data)
     if data["to"] == "bunny"
       EM.system("mplayer #{data['value']}") do |output, status|
-        send_data({:status => status.exitstatus}.to_json)      
-      end
-    end
+	      puts "Done"
+     end
+   end
   end
 end
 
-EventMachine::run {         
-  read = EventMachine.attach(cl, MusicPlayer)  
+cl = UDPSocket.new
+cl.setsockopt(Socket::SOL_SOCKET,Socket::SO_BROADCAST,true)
+cl.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR,true)
+cl.bind('0.0.0.0', 8282)                                
+                           
+
+
+EventMachine::run {
+      
+  read = EventMachine.attach(cl, MusicPlayer) do |c|
+	puts "connect!"
+	end
 }
